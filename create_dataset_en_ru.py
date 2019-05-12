7147717aedfed6c407d497338a8632194d219988
aponamarev <aponamarev@us.imshealth.com>
2019-05-12 15:22:48 -0400

    Created dataset

--- create_dataset_en_ru.py
+++ create_dataset_en_ru.py
@@ -2,12 +2,17 @@ import os
 import subprocess
 import re
 import numpy as np
-import tensorflow as tf
 from tensorflow import keras
+import pickle as pk
 
 
-def data_etl(download_dir: str = ".", file_name: str = "en_ru.tgz", n_lines: int = 3000,
-             num_words: int = 5000) -> dict:
+root_path, file_name = os.path.split(os.path.abspath(__file__))
+data_path = os.path.join(root_path, "data")
+preprocessed_ds_file = os.path.join(data_path, "wlm_en_ru.pkl")
+
+
+def data_etl(download_dir: str = ".", file_name: str = "en_ru.tgz", n_lines: int = None,
+             en_vocab_size: int = 3000, ru_vocab_size: int = 10000) -> dict:
     path = os.path.join(download_dir, file_name)
     en_path = os.path.join(download_dir, "paracrawl-release1.en-ru.zipporah0-dedup-clean.en")
     ru_path = os.path.join(download_dir, "paracrawl-release1.en-ru.zipporah0-dedup-clean.ru")
@@ -38,52 +43,33 @@ def data_etl(download_dir: str = ".", file_name: str = "en_ru.tgz", n_lines: int
 
     print("Extracted successfully")
     # Transformation
-    # remove invalid samples
-    valid_idx = list(
-            filter(lambda idx: 0.5 > len(re.sub("[^а-я]"," ", ru[idx]).split()) / len(ru[idx].split()),
-                   range(len(ru)))
-    )
+    # remove english to english sequences
+    valid_idx = filter(lambda idx: len(re.sub("[^а-я]"," ", ru[idx]).split()) / len(ru[idx].split()) > 0.5,
+                       range(len(ru)))
+    for ds in [en, ru]:
+        # remove short and long sequences based on sequences
+        lengths = list(map(lambda s: len(s.split()), ds))
+        # valid lengths only used for mean and std calculations while lengths used to filter out en and ru datasets
+        valid_lengths = np.fromiter(filter(lambda l: l>3, lengths), dtype=np.int)
+
+        lengths_mean: float = valid_lengths.mean()
+        lengths_std: float = valid_lengths.std()
+
+        # remove extra long sequence as they will skew the loss towards 0
+        length_lim_upper = int(lengths_mean + lengths_std * 1)
+
+        # remove very short sequences
+        length_lim_lower = max(4, int(lengths_mean - lengths_std * 2))
+
+        valid_idx = list(filter(lambda idx: length_lim_lower <= lengths[idx] < length_lim_upper, valid_idx))
 
     # slit numbers
     en = [re.sub("[0-9]", " \g<0>", en[idx]) for idx in valid_idx]
     ru = [re.sub("[0-9]", " \g<0>", ru[idx]) for idx in valid_idx]
 
-    # remove invalid samples
-    # for en
-    lengths = np.array([len(s.split()) for s in en])
-    l_mean = lengths.mean()
-    l_std = lengths.std()
-
-    # remove extra long sequence as they will skew the loss towards 0
-    length_lim_upper = int(l_mean + l_std * 2)
-
-    # remove very short sequences
-    lenght_lim_lower = max(4, int(l_mean - l_std * 2))
-
-    # filter out small samples
-    valid_idx = list(filter(lambda idx: length_lim_upper > len(en[idx].split()) >= lenght_lim_lower, range(len(en))))
-    en = [en[idx] for idx in valid_idx]
-    ru = [ru[idx] for idx in valid_idx]
-
-    # for ru
-    lengths = np.array([len(s.split()) for s in ru])
-    l_mean = lengths.mean()
-    l_std = lengths.std()
-
-    # remove extra long sequence as they will skew the loss towards 0
-    length_lim_upper = int(l_mean + l_std * 2)
-
-    # remove very short sequences
-    lenght_lim_lower = max(4, int(l_mean - l_std * 2))
-
-    # filter out small samples
-    valid_idx = list(filter(lambda idx: length_lim_upper > len(ru[idx].split()) >= lenght_lim_lower, range(len(ru))))
-    en = [en[idx] for idx in valid_idx]
-    ru = [ru[idx] for idx in valid_idx]
-
-    # Tockenize
-    x, x_tk = tokenize(en, num_words=num_words, filters_regex=None)
-    y, y_tk = tokenize(ru, num_words=num_words * 3, filters_regex=None)
+    # Tokenize
+    x, x_tk = tokenize(en, num_words=en_vocab_size, filters_regex=None)
+    y, y_tk = tokenize(ru, num_words=ru_vocab_size, filters_regex=None)
 
     x = pad(x)
     y_length = max([len(sentence) for sentence in y])
@@ -94,17 +80,20 @@ def data_etl(download_dir: str = ".", file_name: str = "en_ru.tgz", n_lines: int
     return {'x': x, 'y': y, 'x_tk': x_tk, 'y_tk': y_tk}
 
 
-def read_lines(from_file: str, n_lines: int) -> list:
+def read_lines(from_file: str, n_lines: int = None) -> list:
     if not os.path.isfile(from_file):
         raise FileNotFoundError(from_file)
 
-    lines: list = []
     with open(from_file, "r") as f:
-        for idx, line in enumerate(f):
-            if idx < n_lines:
-                lines.append(line)
-            else:
-                break
+        if n_lines is not None:
+            lines: list = []
+            for idx, line in enumerate(f):
+                if idx < n_lines:
+                    lines.append(line)
+                else:
+                    break
+        else:
+            lines = [s for s in f]
 
     return lines
 
@@ -117,6 +106,7 @@ def tokenize(x: list, num_words: int = 5000, filters_regex: str = None):
     :param filters_regex: regular expression filter string such as [^a-zA-Z0-9/-]
     :return: Tuple of (tokenized x data, tokenizer used to tokenize x)
     """
+
     if filters_regex is not None:
         x_tk = keras.preprocessing.text.Tokenizer(num_words=num_words, filters=filters_regex, oov_token="[rare]")
     else:
@@ -160,10 +150,32 @@ def id_to_text(idx, tokenizer):
     return ' '.join([index_to_words[prediction] for prediction in idx])
 
 
+def main() -> int:
+
+    dataset = data_etl(download_dir=".", n_lines=50000, en_vocab_size=3500, ru_vocab_size=15000)
+    print("Source(en) first example:")
+    print(id_to_text(dataset['x'][0], dataset['x_tk']))
+    print("Target(ru) first example:")
+    print(id_to_text(dataset['y'][0], dataset['y_tk']))
+    print("Source(en) last example:")
+    print(id_to_text(dataset['x'][-1], dataset['x_tk']))
+    print("Target(ru) last example:")
+    print(id_to_text(dataset['y'][-1], dataset['y_tk']))
+
+    print("x shape: ", dataset['x'].shape)
+    print("y shape: ", dataset['y'].shape)
+
+    if not os.path.isdir(data_path):
+        os.makedirs(data_path)
+
+    with open(preprocessed_ds_file, "wb") as file:
+        pk.dump(dataset, file)
+
+    print("Preprocessed data stored at:", preprocessed_ds_file)
+
+    return 0
 
 if __name__ == '__main__':
 
-    dataset = data_etl(download_dir=".", n_lines=10000, num_words=3000)
-    print(id_to_text(dataset['x'][0], dataset['x_tk']))
-    print(id_to_text(dataset['y'][0], dataset['y_tk']))
-    print("x shape: ", dataset['x'].shape)
+    main()
+
